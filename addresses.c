9b4a9a4020e461978c7ca614c235269ccb224348
unknown <brannongarrett77@gmail.com>
2023-12-12 13:02:02 +0100

    Added more code

--- addresses.c
+++ addresses.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "cs50.h"
 
 int main(void)
 {
     int n = 50;
-    int *p = &n;       // p is a pointer to an integer, and it points to n
-    printf("%p\n", p); // prints the address of n
+    int *p = &n;                                            // p is a pointer to an integer, and it points to n
+    printf("%p is the address on n which stores 50.\n", p); // prints the address of n
+
+    string s = "HI!";
+    printf("%p is the address of the first character stored in s.\n", s); // prints the address of the first character in s
+    printf("%p is the address of the 0 position stored in s.\n", &s[0]);  // prints the address of the first character in s
 }
