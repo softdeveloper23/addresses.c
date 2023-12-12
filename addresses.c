b434782d7989a00bcd0f89c9bfdb33efcdf495b8
unknown <brannongarrett77@gmail.com>
2023-12-12 13:17:30 +0100

    Added a new string

--- addresses.c
+++ addresses.c
@@ -10,4 +10,12 @@ int main(void)
     printf("%s\n", s);                                                    // prints the string
     printf("%p is the address of the first character stored in s.\n", s); // prints the address of the first character in s
     printf("%p is the address of the 0 position stored in s.\n", &s[0]);  // prints the address of the first character in s
+
+    char *t = "HELLO!";
+    printf("%c\n", *t);       // prints the first character in the string
+    printf("%c\n", *(t + 1)); // prints the second character in the string
+    printf("%c\n", *(t + 2)); // prints the third character in the string
+    printf("%c\n", *(t + 3)); // prints the fourth character in the string
+    printf("%c\n", *(t + 4)); // prints the fifth character in the string
+    printf("%c\n", *(t + 5)); // prints the sixth character in the string
 }
